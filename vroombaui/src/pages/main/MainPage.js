9f49f2e98512488a3740e84e3b682ed1e1420562
Andy Grace <54917548+andygrace227@users.noreply.github.com>
2023-08-14 21:29:51 -0700

    added a new RoombaMap component that will allow me to check distance and header information, as well as viz the algorithms

--- vroombaui/src/pages/main/MainPage.js
+++ vroombaui/src/pages/main/MainPage.js
@@ -1,7 +1,35 @@
+import { useState, useEffect, useRef } from "react";
+import RoombaMap from "../../shared/components/RoombaMap";
+
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+
 
 function MainPage() {
+
+    const [roombaPosition, setRoombaPosition] = useState([0,0,0]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRoombaPosition(arr => [arr[0],arr[1], arr[2] + (Math.PI / 180)]);
+            setRoombaPosition(arr => [Math.cos(arr[2]) + 1,Math.sin(arr[2]), arr[2]])
+        }, 16);
+
+
+        return () => clearInterval(interval);
+    }, []);
+
+
     return (
-        <>Welcome to Vroomba.</>
+        <>
+            <>Welcome to Vroomba.</>
+            <RoombaMap width={500} height={500} x={roombaPosition[0]} y={roombaPosition[1]} heading={roombaPosition[2]} ></RoombaMap>
+        </>
     );
   }
   
---
+++ vroombaui/src/shared/components/RoombaMap.js
@@ -0,0 +1,133 @@
+import { useEffect, useState, useRef } from "react";
+
+function RoombaMap(props) {
+  // x in meters,y in meters,heading in radians
+
+  var roombaPosition = [props.x, props.y, props.heading];
+  // center x in meters, center y in meters, scale in pixels per meter
+  const [viewport, setViewport] = useState([1, 0, 250]);
+  const [mouseInfo, setMouseInfo] = useState({ drag: false, x: 0, y: 0, origViewX: 0, origViewY:0});
+  const canvasRef = useRef(null);
+
+  const changeZoom = (e) => {
+    setViewport((arr) => [arr[0], arr[1], Math.min(Math.max(arr[2] - e.deltaY * 0.05, 40), 500)]);
+  };
+
+  const changePosition = (e) => {
+    setMouseInfo((prev)=>{
+        if (prev["drag"] == true) {
+            setViewport((arr) => {
+                console.log((prev.x - e.clientX)/250, (prev.y - e.clientY)/250);
+
+                return [prev.origViewX - (( e.clientX - prev.x) / arr[2]), prev.origViewY - ((prev.y - e.clientY) / arr[2]), arr[2]]});
+        }
+        return prev;
+    })
+    
+  };
+
+  const startDrag = (e) => {
+    setMouseInfo((prevState) => {
+      let newState = { ...prevState };
+      newState.drag = true;
+      newState.x = e.clientX;
+      newState.y = e.clientY;
+      newState.origViewX = viewport[0];
+      newState.origViewY = viewport[1];
+      console.log(e.x, e.y);
+      return newState;
+    });
+  };
+
+  const endDrag = (e) => {
+    setMouseInfo((prevState) => {
+    let newState = { ...prevState };
+    newState.drag = false;
+    newState.x = e.clientX;
+    newState.y = e.clientY;
+    return newState;
+    });
+  };
+
+  var canvas = (
+    <canvas
+      ref={canvasRef}
+      width={props.width}
+      height={props.height}
+      onWheel={changeZoom}
+      onMouseDown={startDrag}
+      onMouseUp={endDrag}
+      onMouseMove={changePosition}
+    ></canvas>
+  );
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const screen = [canvas.width, canvas.height];
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    renderRoomba(ctx, roombaPosition, viewport, screen);
+    renderGrid(ctx, viewport, screen);
+  }, [props.x, props.y, props.heading]);
+
+  return canvas;
+}
+
+function translateCoords(coords, viewport, screen) {
+  // From the unit to the screen... how fun.
+  var viewport_translation = [viewport[2] * (coords[0] - viewport[0]), viewport[2] * (coords[1] - viewport[1])];
+  return [viewport_translation[0] + screen[0] / 2, -1.0 * viewport_translation[1] + screen[1] / 2];
+}
+
+function scaleNumber(num, viewport) {
+  return viewport[2] * num;
+}
+
+function renderRoomba(ctx, position, viewport, screen) {
+  ctx.lineCap = "round";
+  ctx.beginPath();
+  var heading = Math.PI / 2 + position[2];
+  var sscoords = translateCoords([position[0], position[1]], viewport, screen);
+  var hscoords = translateCoords(
+    [position[0] + 0.34 * Math.cos(heading), position[1] + 0.34 * Math.sin(heading)],
+    viewport,
+    screen
+  );
+
+  ctx.lineWidth = 10;
+
+  ctx.arc(
+    sscoords[0],
+    sscoords[1],
+    viewport[2] * 0.34,
+    -1.0 * heading + (5 * Math.PI) / 4 - Math.PI,
+    -1.0 * heading - (5 * Math.PI) / 4 - Math.PI
+  );
+  ctx.stroke();
+
+  ctx.lineWidth = 7;
+  ctx.beginPath();
+  ctx.moveTo(sscoords[0], sscoords[1]);
+  ctx.lineTo(hscoords[0], hscoords[1]);
+  ctx.stroke();
+}
+
+function renderGrid(ctx, viewport, screen) {
+  ctx.lineWidth = 0.5;
+
+  for (var i = -10; i < 10; i++) {
+    ctx.beginPath();
+    ctx.moveTo(...translateCoords([i, -10], viewport, screen));
+    ctx.lineTo(...translateCoords([i, 10], viewport, screen));
+    ctx.stroke();
+  }
+  for (var j = -10; j < 10; j++) {
+    ctx.beginPath();
+    ctx.moveTo(...translateCoords([-10, j], viewport, screen));
+    ctx.lineTo(...translateCoords([10, j], viewport, screen));
+    ctx.stroke();
+  }
+}
+
+export default RoombaMap;
